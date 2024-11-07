013151d9a59fcbb7cf059ee9d7f21ccab296684f
padekarjigna <jignapadekar@gmail.com>
2024-11-07 14:15:47 +0530

    Changed backend.tf

--- Hotstar-Clone-main/EKS_TERRAFORM/backend.tf
+++ Hotstar-Clone-main/EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bijanhotstar" # Replace with your actual S3 bucket name
+    bucket = "hotstar-bucket" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
